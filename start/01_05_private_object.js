75df5f18382b9c9721a0f24abec488ea7ebd1256
kpat94 <pkkinage@gmail.com>
2026-07-31 12:55:15 +0000

    Usage of Symbols.

--- start/01_05_private_object.js
+++ start/01_05_private_object.js
@@ -1,1 +1,12 @@
 // Write your code here
+const username = Symbol("username");
+const password = Symbol("password");
+
+const user = {
+  [username]: "kparth",
+  [password]: "1241351",
+  age: 27
+}
+
+console.log(user.username);
+console.log(user.password);
